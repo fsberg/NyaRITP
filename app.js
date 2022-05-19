24d69a3a80279949df1d73fd2d85901e670f24c5
Fredrik Sloberg <sloberg@gmail.com>
2022-05-19 12:56:03 +0200

    massa böös

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .nav');
 let header = document.querySelector('.header');
 
+
+let fel_btn = document.querySelector('#fel-btn');
+let felform = document.querySelector('.support .box-container .fel-form');
+
+let closeform = document.querySelector('#closeform');
+
+
+
+
+
+// mobil meny
+
 menu.onclick = () =>{
    menu.classList.toggle('fa-times');
    navbar.classList.toggle('active');
@@ -18,4 +30,20 @@ window.onscroll = () =>{
      header.classList.remove('active');
   }
 
+}
+
+// supportknappar
+
+
+
+
+fel_btn.onclick = () =>{
+   console.log('on');
+   felform.classList.toggle('active');
+   
+}
+
+closeform.onclick = () =>{
+   console.log('stänger fromen');
+   felform.classList.toggle('active');
 }
