a9c386bba456ea52b23b00f7f13673c74db5b3c2
Fredrik Sloberg <sloberg@gmail.com>
2022-05-19 13:28:45 +0200

    support knappar

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .nav');
 let header = document.querySelector('.header');
 
-
-let fel_btn = document.querySelector('#fel-btn');
-let felform = document.querySelector('.support .box-container .fel-form');
-
 let closeform = document.querySelector('#closeform');
 
+let it_btn = document.querySelector('#it-btn');
+let itform = document.querySelector('.support .box-container .form');
+
+let fel_btn = document.querySelector('#fel-btn');
+let felform = document.querySelector('.support .box-container .form');
 
 
+let sup_btn = document.querySelector('#sup-btn');
+let supform = document.querySelector('.support .box-container .form');
 
+let count_btn = document.querySelector('#count-btn');
+let countform = document.querySelector('.support .box-container .form');
 
 // mobil meny
 
@@ -35,15 +40,48 @@ window.onscroll = () =>{
 // supportknappar
 
 
+it_btn.onclick = () =>{
+   console.log('öppnar IT');
+   itform.classList.toggle('active');
+   
+}
+
+closeform.onclick = () =>{
+   console.log('stänger');
+   itform.classList.toggle('active');
+}
+
 
 
 fel_btn.onclick = () =>{
-   console.log('on');
+   console.log('öppnar felanmälan');
    felform.classList.toggle('active');
    
 }
 
 closeform.onclick = () =>{
-   console.log('stänger fromen');
+   console.log('stänger');
    felform.classList.toggle('active');
+}
+
+sup_btn.onclick = () =>{
+   console.log('öppnar toner');
+   supform.classList.toggle('active');
+   
+}
+
+closeform.onclick = () =>{
+   console.log('stänger');
+   supform.classList.toggle('active');
+}
+
+count_btn.onclick = () =>{
+   console.log('öppnar mätarställning');
+   countform.classList.toggle('active');
+   
+}
+
+closeform.onclick = () =>{
+   console.log('stänger');
+   countform.classList.toggle('active');
 }
